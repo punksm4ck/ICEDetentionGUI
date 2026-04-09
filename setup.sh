f5d52a14d3b2e64d82e66696d3b984c2f15c769d
punksm4ck <punksm4ck@gmail.com>
2026-04-08 22:56:37 -0700

    Sovereignty: Finalize Punksm4ck branding

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 "© 2026 Punksm4ck. All rights reserved."
 "© 2026 Punksm4ck. All rights reserved."
+"© 2026 Punksm4ck. All rights reserved."
 #!/bin/bash
 echo " Setting up ICE Detention Center Tracker            "
 echo "===================================================="
